3dabcf399a1691fe17e1ac85ce0358681882a852
dahiana5-star <mendietadahiana1@gmail.com>
2024-10-20 17:05:38 -0300

    Cambios en la BD

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Tarjeta)
+admin.site.register(UsuarioTarjeta)
+admin.site.register(EstadoEspacio)
+admin.site.register(Espacio)
+admin.site.register(UsuarioEspacio)
+admin.site.register(Tablero)
+admin.site.register(Estado)
+admin.site.register(EstadoTarjeta)
+admin.site.register(Subtarea)
+
